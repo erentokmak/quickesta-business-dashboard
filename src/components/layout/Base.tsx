8f2baa25250c87f5043a9236aa239df469767d4f
Eren Tokmak <eren.tokmak@outlook.com>
2025-02-16 00:41:56 +0300

    feat: deleted animation

--- src/components/layout/Base.tsx
+++ src/components/layout/Base.tsx
@@ -1,23 +1,11 @@
-import { motion } from 'framer-motion'
 import React from 'react'
-import { slideAnimation, getTransition } from '@/utils/animations'
 
 interface BaseProps {
   children: React.ReactNode
 }
 
 const Base: React.FC<BaseProps> = ({ children }) => {
-  return (
-    <motion.div
-      variants={slideAnimation}
-      initial="initial"
-      animate="animate"
-      exit="exit"
-      transition={getTransition()}
-    >
-      {children}
-    </motion.div>
-  )
+  return <div>{children}</div>
 }
 
 export default Base
--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,24 +1,11 @@
-import { motion } from 'framer-motion'
 import React from 'react'
-import { slideAnimation, getTransition } from '@/utils/animations'
 
 interface MainProps {
   children: React.ReactNode
 }
 
 const Main: React.FC<MainProps> = ({ children }) => {
-  return (
-    <motion.main
-      variants={slideAnimation}
-      initial="initial"
-      animate="animate"
-      exit="exit"
-      transition={getTransition(0.2)}
-      role="content"
-    >
-      {children}
-    </motion.main>
-  )
+  return <main>{children}</main>
 }
 
 export default Main
